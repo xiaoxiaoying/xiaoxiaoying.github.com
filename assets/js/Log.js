933d2c047670e086f27cca774f4d3256dc6a8b03
小小蝇 <xiaoxiaoying91@gmail.com>
2017-10-24 15:42:23 +0800

    添加toast

--- assets/js/Log.js
+++ assets/js/Log.js
@@ -17,3 +17,46 @@ var Log = {
 
 
 };
+var Utils = {
+    /**
+     * toast
+     */
+
+    Toast: function (msg, duration, position) {
+        duration = isNaN(duration) ? 900 : duration;
+        if (Utils.isNull(position)) {
+            position = '80%';
+        }
+        var m = document.createElement('div');
+        m.innerHTML = msg;
+        m.className = "toast";
+        m.id = "t_id";
+        document.body.appendChild(m);
+        var t = document.getElementById("t_id");
+        m.style.left = (Utils.windowWidth() - t.clientWidth) / 2 + "px";
+        m.style.top = position;
+        setTimeout(function () {
+            var d = 0.5;
+            m.style.webkitTransition = '-webkit-transform ' + d + 's ease-in, opacity ' + d + 's ease-in';
+            m.style.opacity = '0';
+            setTimeout(function () {
+                document.body.removeChild(m)
+            }, d * 1000);
+        }, duration);
+    },
+
+    windowWidth: function () {
+        return window.innerWidth;
+    },
+    windowHeight: function () {
+        return window.innerHeight;
+    },
+
+    isNull: function (key) {
+        if (key === undefined || key.length <= 0 || key === null) {
+            return true;
+        }
+        return false;
+    }
+
+};
